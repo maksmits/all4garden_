312088c01e25dae156c5a07f38d16da71e0110ac
maksmits <113168289+maksmits@users.noreply.github.com>
2023-06-23 09:16:11 +0200

    add main page

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/main.css">
+    <title>All4Garden</title>
+</head>
+<body>
+<?php
+require('./components/Header/index.php');
+require('./components/Heroes/index.php');
+require('./components/Footer/footer.php')
+?>
+<span style="height: 64px; width: 64px; margin-left: 0px; margin-right: auto; margin-top: 0px; right: 35px; bottom: 60px; padding: 12px; background-image: none;" class="u-back-to-top u-grey-90 u-icon u-icon-rounded u-opacity u-opacity-85 u-radius-14" data-href="#">
+        <svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 551.13 551.13"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-1d98"></use></svg>
+        <svg class="u-svg-content" enable-background="new 0 0 551.13 551.13" viewBox="0 0 551.13 551.13" xmlns="http://www.w3.org/2000/svg" id="svg-1d98"><path d="m275.565 189.451 223.897 223.897h51.668l-275.565-275.565-275.565 275.565h51.668z"></path></svg>
+    </span>
+</body>
+</html>
